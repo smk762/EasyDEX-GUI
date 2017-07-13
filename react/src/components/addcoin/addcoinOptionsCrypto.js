74e0d01c93b2aed4eafedc3490b399be5d4e04f8
pbca26 <pbca26@gmail.com>
2017-07-13 03:13:49 -0700

    disable full mode on win

--- react/src/components/addcoin/addcoinOptionsCrypto.js
+++ react/src/components/addcoin/addcoinOptionsCrypto.js
@@ -11,24 +11,50 @@ class AddCoinOptionsCrypto extends React.Component {
   }
 
   render() {
+    const isWindows = this.props.appSettings && this.props.appSettings.appInfo && this.props.appSettings.appInfo.sysInfo && this.props.appSettings.appInfo.sysInfo.platform === 'win32';
+
     //<option value="ANC|full">AnonCoin (ANC)</option>
     //<option value="MZC|full">MazaCoin (MZC)</option>
     //<option value="SYS|full">SysCoin (SYS)</option>
     return (
-      <optgroup label="Crypto Currencies">
-        <option value="BTCD|full" className={ this.state.nativeOnly ? 'hide' : '' }>BitcoinDark (BTCD)</option>
-        <option value="BTC|full|basilisk" className={ this.state.nativeOnly ? 'hide' : '' }>Bitcoin (BTC)</option>
-        <option value="BTM|full" className={ this.state.nativeOnly ? 'hide' : '' }>Bitmark (BTM)</option>
-        <option value="CARB|full" className={ this.state.nativeOnly ? 'hide' : '' }>Carboncoin (CARB)</option>
-        <option value="DGB|full" className={ this.state.nativeOnly ? 'hide' : '' }>Digibyte (DGB)</option>
-        <option value="DOGE|full" className={ this.state.nativeOnly ? 'hide' : '' }>Dogecoin (DOGE)</option>
-        <option value="FRK|full" className={ this.state.nativeOnly ? 'hide' : '' }>Franko (FRK)</option>
-        <option value="GAME|full" className={ this.state.nativeOnly ? 'hide' : '' }>Gamecredits (GAME)</option>
+      <optgroup label={ translate('ADD_COIN.CRYPTO_CURRENCIES') }>
+        <option
+          value="BTCD|full"
+          className={ this.state.nativeOnly || isWindows ? 'hide' : '' }>BitcoinDark (BTCD)</option>
+        <option
+          value="BTC|full"
+          className={ this.state.nativeOnly || isWindows ? 'hide' : '' }>Bitcoin (BTC)</option>
+        <option
+          value="BTM|full"
+          className={ this.state.nativeOnly || isWindows ? 'hide' : '' }>Bitmark (BTM)</option>
+        <option
+          value="CARB|full"
+          className={ this.state.nativeOnly || isWindows ? 'hide' : '' }>Carboncoin (CARB)</option>
+        <option
+          value="DGB|full"
+          className={ this.state.nativeOnly || isWindows ? 'hide' : '' }>Digibyte (DGB)</option>
+        <option
+          value="DOGE|full"
+          className={ this.state.nativeOnly || isWindows ? 'hide' : '' }>Dogecoin (DOGE)</option>
+        <option
+          value="FRK|full"
+          className={ this.state.nativeOnly || isWindows ? 'hide' : '' }>Franko (FRK)</option>
+        <option
+          value="GAME|full"
+          className={ this.state.nativeOnly || isWindows ? 'hide' : '' }>Gamecredits (GAME)</option>
         <option value="KMD|basilisk|native">Komodo (KMD)</option>
-        <option value="LTC|full" className={ this.state.nativeOnly ? 'hide' : '' }>Litecoin (LTC)</option>
-        <option value="UNO|full" className={ this.state.nativeOnly ? 'hide' : '' }>Unobtanium (UNO)</option>
-        <option value="ZEC|full" className={ this.state.nativeOnly ? 'hide' : '' }>Zcash (ZEC)</option>
-        <option value="ZET|full" className={ this.state.nativeOnly ? 'hide' : '' }>Zetacoin (ZET)</option>
+        <option
+          value="LTC|full"
+          className={ this.state.nativeOnly || isWindows ? 'hide' : '' }>Litecoin (LTC)</option>
+        <option
+          value="UNO|full"
+          className={ this.state.nativeOnly || isWindows ? 'hide' : '' }>Unobtanium (UNO)</option>
+        <option
+          value="ZEC|full"
+          className={ this.state.nativeOnly || isWindows ? 'hide' : '' }>Zcash (ZEC)</option>
+        <option
+          value="ZET|full"
+          className={ this.state.nativeOnly || isWindows ? 'hide' : '' }>Zetacoin (ZET)</option>
       </optgroup>
     );
   }
