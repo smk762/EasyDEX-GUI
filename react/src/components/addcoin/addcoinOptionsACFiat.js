0166259c3c8540f5f0530d0d6f5340505b4c5775
pbca26 <pbca26@gmail.com>
2017-08-28 21:59:05 +0300

    refactor addcoin ac render

--- react/src/components/addcoin/addcoinOptionsACFiat.js
+++ react/src/components/addcoin/addcoinOptionsACFiat.js
@@ -2,49 +2,49 @@ import React from 'react';
 import { translate } from '../../translate/translate';
 
 class AddCoinOptionsACFiat extends React.Component {
-  const _fiat = [
-    'aud',
-    'brl',
-    'gbp',
-    'bgn',
-    'cad',
-    'hrk',
-    'czk',
-    'cny',
-    'dkk',
-    'eur',
-    'hkd',
-    'huf',
-    'inr',
-    'idr',
-    'ils',
-    'jpy',
-    'krw',
-    'myr',
-    'mxn',
-    'nzd',
-    'nok',
-    'php',
-    'pln',
-    'ron',
-    'rub',
-    'sgd',
-    'zar',
-    'sek',
-    'chf',
-    'thb',
-    'try',
-    'usd'
-  ];
-  let _items = [];
-
-  for (let i = 0; i < _fiat.length; i++) {
-    _items.push(
-      <option value={ `${_fiat[i].toUpperCase()}|basilisk|native` }>{ translate(`FIAT_CURRENCIES.${_fiat[i].toUpperCase()}`) }</option>
-    );
-  }
-
   render() {
+    const _fiat = [
+      'aud',
+      'brl',
+      'gbp',
+      'bgn',
+      'cad',
+      'hrk',
+      'czk',
+      'cny',
+      'dkk',
+      'eur',
+      'hkd',
+      'huf',
+      'inr',
+      'idr',
+      'ils',
+      'jpy',
+      'krw',
+      'myr',
+      'mxn',
+      'nzd',
+      'nok',
+      'php',
+      'pln',
+      'ron',
+      'rub',
+      'sgd',
+      'zar',
+      'sek',
+      'chf',
+      'thb',
+      'try',
+      'usd'
+    ];
+    let _items = [];
+
+    for (let i = 0; i < _fiat.length; i++) {
+      _items.push(
+        <option value={ `${_fiat[i].toUpperCase()}|basilisk|native` }>{ translate(`FIAT_CURRENCIES.${_fiat[i].toUpperCase()}`) }</option>
+      );
+    }
+
     return (
       <optgroup label={ translate('ADD_COIN.FIAT_CURRENCIES') }>
       { _items }
