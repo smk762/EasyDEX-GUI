28334f4ba8d67e857564b5902fd0c38ef0cd4cbf
Miika Turunen <miika.turunen@gmail.com>
2017-09-18 01:31:18 +0300

    AGP-228, clean up jsx

--- react/src/components/dashboard/settings/settings.render.js
+++ react/src/components/dashboard/settings/settings.render.js
@@ -18,100 +18,94 @@ import WalletBackupPanel from './settings.walletBackupPanel';
 
 export const SettingsRender = function() {
   return (
-    <div className="margin-left-0 full-height">
-      <div
-        className="page-content full-height"
-        id="section-iguana-wallet-settings">
+    <div 
+      id="section-iguana-wallet-settings"
+      className="padding-30">
         <div className="row">
-          <div className="col-xlg-12 col-md-12">
-            <div className="row">
-              <div className="col-xlg-12 col-md-12">
-                <h4 className="font-size-14 text-uppercase">{ translate('INDEX.WALLET_SETTINGS') }</h4>
-                <Panel
-                  uniqId={'SettingsAccordion'}
-                  singleOpen={true}>
-                  { !this.props.disableWalletSpecificUI &&
-                    <PanelSection
-                      title={ translate('INDEX.WALLET_INFO') }
-                      icon="icon md-balance-wallet"
-                      openByDefault={!this.props.disableWalletSpecificUI}>
-                      <WalletInfoPanel />
-                    </PanelSection>
-                  }
-                  { !this.props.disableWalletSpecificUI &&
-                    <PanelSection
-                      title={ translate('INDEX.ADD_NODE') }
-                      icon="icon md-plus-square">
-                      <AddNodePanel />
-                    </PanelSection>
-                  }
-                  { !this.props.disableWalletSpecificUI &&
-                    <PanelSection
-                      title={ translate('INDEX.WALLET_BACKUP') }
-                      icon="icon wb-briefcase">
-                      <WalletBackupPanel />
-                    </PanelSection>
-                  }
-                  { !this.props.disableWalletSpecificUI &&
-                    <PanelSection
-                      title={ translate('INDEX.FIAT_CURRENCY') }
-                      icon="icon fa-money">
-                      <FiatCurrencyPanel />
-                    </PanelSection>
-                  }
-                  { !this.props.disableWalletSpecificUI &&
-                    <PanelSection
-                      title={ translate('INDEX.EXPORT_KEYS') }
-                      icon="icon md-key">
-                      <ExportKeysPanel />
-                    </PanelSection>
-                  }
-                  { !this.props.disableWalletSpecificUI &&
-                    <PanelSection
-                      title={ translate('INDEX.IMPORT_KEYS') }
-                      icon="icon md-key">
-                      <ImportKeysPanel />
-                    </PanelSection>
-                  }
-                    <PanelSection
-                      title={ translate('INDEX.DEBUG_LOG') }
-                      icon="icon fa-bug"
-                      openByDefault={this.props.disableWalletSpecificUI}>                    
-                      <DebugLogPanel />
-                    </PanelSection>
-                    <PanelSection
-                        title={ translate('SETTINGS.APP_CONFIG') + ' (config.json)' }
-                        icon="icon fa-bug">
-                        <AppSettingsPanel />
-                    </PanelSection>
-                    <PanelSection
-                      title={ translate('SETTINGS.APP_INFO') }
-                      icon="icon md-info">
-                      <AppInfoPanel />
-                    </PanelSection>
-                  { this.props.Main && this.props.Main.coins.native &&
-                    <PanelSection
-                      title="CLI"
-                      icon="icon fa-code">
-                      <CliPanel />
-                    </PanelSection>
-                  }
-                    <PanelSection
-                      title={ translate('INDEX.UPDATE') }
-                      icon="icon fa fa-cloud-download">
-                      <AppUpdatePanel />
-                    </PanelSection>
-                    <PanelSection
-                      title="Support"
-                      icon="icon fa fa-life-ring">
-                      <SupportPanel />
-                    </PanelSection>
-                </Panel>
-              </div>
-            </div>
+          <div className="col-sm-12">
+            <h4 className="font-size-14 text-uppercase">{ translate('INDEX.WALLET_SETTINGS') }</h4>
+            <Panel
+              uniqId={'SettingsAccordion'}
+              singleOpen={true}>
+              { !this.props.disableWalletSpecificUI &&
+                <PanelSection
+                  title={ translate('INDEX.WALLET_INFO') }
+                  icon="icon md-balance-wallet"
+                  openByDefault={!this.props.disableWalletSpecificUI}>
+                  <WalletInfoPanel />
+                </PanelSection>
+              }
+              { !this.props.disableWalletSpecificUI &&
+                <PanelSection
+                  title={ translate('INDEX.ADD_NODE') }
+                  icon="icon md-plus-square">
+                  <AddNodePanel />
+                </PanelSection>
+              }
+              { !this.props.disableWalletSpecificUI &&
+                <PanelSection
+                  title={ translate('INDEX.WALLET_BACKUP') }
+                  icon="icon wb-briefcase">
+                  <WalletBackupPanel />
+                </PanelSection>
+              }
+              { !this.props.disableWalletSpecificUI &&
+                <PanelSection
+                  title={ translate('INDEX.FIAT_CURRENCY') }
+                  icon="icon fa-money">
+                  <FiatCurrencyPanel />
+                </PanelSection>
+              }
+              { !this.props.disableWalletSpecificUI &&
+                <PanelSection
+                  title={ translate('INDEX.EXPORT_KEYS') }
+                  icon="icon md-key">
+                  <ExportKeysPanel />
+                </PanelSection>
+              }
+              { !this.props.disableWalletSpecificUI &&
+                <PanelSection
+                  title={ translate('INDEX.IMPORT_KEYS') }
+                  icon="icon md-key">
+                  <ImportKeysPanel />
+                </PanelSection>
+              }
+                <PanelSection
+                  title={ translate('INDEX.DEBUG_LOG') }
+                  icon="icon fa-bug"
+                  openByDefault={this.props.disableWalletSpecificUI}>                    
+                  <DebugLogPanel />
+                </PanelSection>
+                <PanelSection
+                    title={ translate('SETTINGS.APP_CONFIG') + ' (config.json)' }
+                    icon="icon fa-bug">
+                    <AppSettingsPanel />
+                </PanelSection>
+                <PanelSection
+                  title={ translate('SETTINGS.APP_INFO') }
+                  icon="icon md-info">
+                  <AppInfoPanel />
+                </PanelSection>
+              { this.props.Main && this.props.Main.coins.native &&
+                <PanelSection
+                  title="CLI"
+                  icon="icon fa-code">
+                  <CliPanel />
+                </PanelSection>
+              }
+                <PanelSection
+                  title={ translate('INDEX.UPDATE') }
+                  icon="icon fa fa-cloud-download">
+                  <AppUpdatePanel />
+                </PanelSection>
+                <PanelSection
+                  title="Support"
+                  icon="icon fa fa-life-ring">
+                  <SupportPanel />
+                </PanelSection>
+            </Panel>
           </div>
         </div>
       </div>
-    </div>
   );
 };
