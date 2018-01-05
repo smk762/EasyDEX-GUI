2afd8828f4346144e747ffeef82305f467b96305
pbca26 <pbca26@gmail.com>
2018-01-05 14:32:31 +0300

    coin tile badge pos change css

--- react/src/components/dashboard/coinTile/coinTileItem.render.js
+++ react/src/components/dashboard/coinTile/coinTileItem.render.js
@@ -6,6 +6,9 @@ const CoinTileItemRender = function() {
 
   return (
     <div className="list-group-item col-xlg-6 col-lg-12 wallet-widgets-info pointer">
+      <span className={ `badge up badge-${item.modecolor}` }>
+        { item.modecode }
+      </span>
       <div className={ 'widget widget-shadow' + (this.props.ActiveCoin.coin === item.coin ? ' active' : '') }>
         <div
           className="widget-content text-center bg-white padding-20"
@@ -15,9 +18,6 @@ const CoinTileItemRender = function() {
               className="img-responsive"
               src={ `assets/images/cryptologo/${item.coinlogo.toLowerCase()}.png` }
               alt={ item.coinname }/>
-            <span className={ `badge up badge-${item.modecolor}` }>
-              { item.modecode }
-            </span>
           </a>
           <div className="coin-name">
             { item.coinname } ({ item.coinlogo.toUpperCase() })
