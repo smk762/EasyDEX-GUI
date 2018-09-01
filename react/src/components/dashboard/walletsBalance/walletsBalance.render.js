90b048c19ee930ee6a5105a279ddd61b25b9637d
pbca26 <pbca26@gmail.com>
2018-09-01 11:52:02 +0300

    balance widget fix width

--- react/src/components/dashboard/walletsBalance/walletsBalance.render.js
+++ react/src/components/dashboard/walletsBalance/walletsBalance.render.js
@@ -15,7 +15,7 @@ const WalletsBalanceRender = function() {
             this.props.ActiveCoin.coin === 'CHIPS' ||
             (this.props.ActiveCoin.mode === 'spv' && this.props.ActiveCoin.coin !== 'KMD') ||
             this.renderBalance('total') === this.renderBalance('transparent') ||
-            this.renderBalance('total') === 0 ? 'col-lg-12 col-xs-12 balance-placeholder--bold' : 'col-lg-3 col-xs-12'
+            this.renderBalance('total') === 0 ? 'col-lg-12 col-xs-12 balance-placeholder--bold' : (this.props.ActiveCoin.mode === 'spv' || Number(this.renderBalance('private')) === 0 ? 'col-lg-4 col-xs-12' : 'col-lg-3 col-xs-12')
           }>
             <div className="widget widget-shadow">
               <div className="widget-content">
@@ -87,7 +87,7 @@ const WalletsBalanceRender = function() {
             </div>
           </div>
 
-          <div className={ this.props.ActiveCoin.coin === 'KMD' && Number(this.renderBalance('interest')) > 0 ? 'col-lg-3 col-xs-12' : 'hide' }>
+          <div className={ this.props.ActiveCoin.coin === 'KMD' && Number(this.renderBalance('interest')) > 0 ? (this.props.ActiveCoin.mode === 'spv' || Number(this.renderBalance('private')) === 0 ? 'col-lg-4 col-xs-12' : 'col-lg-3 col-xs-12') : 'hide' }>
             <div className="widget widget-shadow">
               <div className="widget-content">
                 <div className="padding-20 padding-top-10">
@@ -114,7 +114,7 @@ const WalletsBalanceRender = function() {
             this.props.ActiveCoin.coin === 'CHIPS' ||
             (this.props.ActiveCoin.coin !== 'KMD' && this.props.ActiveCoin.mode === 'spv') ||
             Number(this.renderBalance('total')) === 0 ||
-            this.renderBalance('total') === this.renderBalance('transparent') ? 'hide' : 'col-lg-3 col-xs-12'
+            this.renderBalance('total') === this.renderBalance('transparent') ? 'hide' : (this.props.ActiveCoin.mode === 'spv' || Number(this.renderBalance('private')) === 0 ? 'col-lg-4 col-xs-12' : 'col-lg-3 col-xs-12')
           }>
             <div className="widget widget-shadow">
               <div className="widget-content">
