d5511e1734f0cab45e47e5bc618b25ae0c322e6c
pbca26 <pbca26@gmail.com>
2018-09-26 17:52:54 +0300

    possible electron remote object ref render breaking error fix #2

--- react/src/components/dashboard/settings/settings.addressBookPanel.js
+++ react/src/components/dashboard/settings/settings.addressBookPanel.js
@@ -20,7 +20,9 @@ const prepCoinsList = () => {
   for (let i = 0; i < coins.length; i++) {
     if (Config.experimentalFeatures ||
         (!Config.experimentalFeatures && (_coins[i] === 'KMD' || _coins[i] === 'CHIPS'))) {
-      if (mainWindow.electrumServers[coins[i].toLowerCase()] &&
+      if (mainWindow &&
+        mainWindow.electrumServers &&
+        mainWindow.electrumServers[coins[i].toLowerCase()] &&
           coins[i] !== 'CHIPS') {
         _coins.push(coins[i]);
       }
