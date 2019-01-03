5b15df0889e585c51686a5f4a33ef313128f74cc
pbca26 <pbca26@gmail.com>
2019-01-03 12:42:00 +0300

    coin tile erc20 badge color bug fix

--- react/src/components/dashboard/coinTile/coinTileItem.render.js
+++ react/src/components/dashboard/coinTile/coinTileItem.render.js
@@ -20,8 +20,8 @@ const CoinTileItemRender = function() {
   
   return (
     <div className="list-group-item col-xlg-6 col-lg-12 wallet-widgets-info pointer">
-      <span className={ `badge up badge-${!erc20ContractId[item.coin] ? item.modecolor : 'success'}` }>
-        { erc20ContractId[item.coin] ? 'ERC20' : item.modecode }
+      <span className={ `badge up badge-${item.mode === 'eth' && erc20ContractId[item.coin] ? 'success' : item.modecolor}` }>
+        { item.mode === 'eth' && erc20ContractId[item.coin] ? 'ERC20' : item.modecode }
       </span>
       <div className={ 'widget widget-shadow' + (this.props.ActiveCoin.coin === item.coin ? ' active' : '') }>
         <div
