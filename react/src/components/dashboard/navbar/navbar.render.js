170b756f1ee6c8c88cb74ee1e5d0382a314b0fb9
pbca26 <pbca26@gmail.com>
2019-01-22 13:11:16 +0300

    dev mode flag update

--- react/src/components/dashboard/navbar/navbar.render.js
+++ react/src/components/dashboard/navbar/navbar.render.js
@@ -85,7 +85,7 @@ const NavbarRender = function() {
               </li>
             }
             { Config.experimentalFeatures &&
-              Config.dev &&
+              (Config.dev || staticVar.argv.indexOf('devmode') > -1) &&
               _activeCoin &&
               _activeCoin.mode === 'native' &&
               <li className={ 'nav-top-menu' + (this.isSectionActive('dice') ? ' active' : '') }>
